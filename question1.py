6d5bf8765754140eca1f2d812363f00f2a0424c7
Bethel O <bethel.olowofela@gmail.com>
2026-02-10 15:53:30 +0000

    Initial commit

---
+++ question1.py
@@ -0,0 +1,13 @@
+"""
+Question 1 module.
+Contains a simple function and executable behaviour.
+"""
+
+
+def alwaysTrue():
+    """Return True."""
+    return True
+
+
+if __name__ == "__main__":
+    print("The answer is:", alwaysTrue())
